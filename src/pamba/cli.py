327ca7d8acad436694f122eb1f1dad42dc69b741
Peter Sobolewski <76622105+psobolewskiPhD@users.noreply.github.com>
2022-12-18 21:15:38 +0100

    feat: if dry-run, pass the flag to conda for more info (#8)
    * if dry-run, pass the flag to conda for more info
    
    * use pip --dry-run too
    
    * add a print
    
    * Update src/pamba/cli.py
    
    Co-authored-by: Talley Lambert <talley.lambert@gmail.com>
    
    Co-authored-by: Talley Lambert <talley.lambert@gmail.com>

--- src/pamba/cli.py
+++ src/pamba/cli.py
@@ -165,10 +165,16 @@ def install(args: Namespace, conda_args: Optional[List[str]] = None) -> None:
             print("Would install from conda:")
             for req in from_conda:
                 print(f"  - {req}")
+            print("conda dry-run output:")
+            conda_args = conda_args or []
+            conda_args.extend(["--dry-run", "--no-banner"])
+            conda_install(from_conda, extra_args=conda_args)
         if from_pip:
             print("Would install from pip:")
             for req in from_pip:
                 print(f"  - {req}")
+            print("pip dry-run output:")
+            pip_install([r.replace(" ", "") for r in from_pip], ["--dry-run"])
     else:
         if from_conda:
             print("installing conda deps")
